adffd0b9d0cd3e369ac8956ed17b30d562c9bcb9
Nataliya Kim <magistr1982@gmail.com>
2023-10-09 14:59:10 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/royal-estate/',
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+    },
+  },
+});
