f97344a6bf2889b8734769bf5fa018f389848936
Nataliya Kim <magistr1982@gmail.com>
2023-10-09 18:48:08 +0300

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
 const App = () => {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="royal-estate/" element={<Home />} />
+        <Route path="royal-estate/about" element={<About />} />
+        <Route path="royal-estate/profile" element={<Profile />} />
+        <Route path="royal-estate/signin" element={<Signin />} />
+        <Route path="royal-estate/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
