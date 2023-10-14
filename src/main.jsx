0661e282dc1422fc43d00e4c11fe7ce12a76c528
Nataliya Kim <magistr1982@gmail.com>
2023-10-14 07:55:27 +0300

    Add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { store } from './redux/store.js';
+import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter
       // basename="/royal-estate/"
-    >
-      <App />
-    </BrowserRouter>
+      >
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
