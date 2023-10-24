ed04d5611657ae2f6d20192bfdcb8aab625be90b
Nataliya Kim <magistr1982@gmail.com>
2023-10-24 09:34:36 +0300

    update header search form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
 import { logo } from '../assets/icons';
+import { useState, useEffect } from 'react';
 import { FaSistrix, FaBars } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
   const { currentUser } = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <section className="bg-slate-300  shadow-md">
       <header className="max-container py-1 padding-x mx-auto">
@@ -12,13 +31,20 @@ const Header = () => {
           <a href="/">
             <img src={logo} alt="royal estate logo" width={100} height={60} />
           </a>
-          <form className="bg-slate-100 p-2 rounded-md flex items-center">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-slate-100 p-2 rounded-md flex items-center"
+          >
             <input
+              value={searchTerm}
+              onChange={event => setSearchTerm(event.target.value)}
               type="text"
               placeholder="Search..."
               className="bg-transparent info-text focus:outline-none w-full md:min-w-[320px]"
             />
-            <FaSistrix className="text-primary w-[20px] h-[20px] cursor-pointer" />
+            <button>
+              <FaSistrix className="text-primary w-[20px] h-[20px] cursor-pointer" />
+            </button>
           </form>
           <nav>
             <ul className="flex items-center gap-6 max-md:hidden">
